6f8882048b85b1e5392b02796e83cf6eacdce06e
Hugo Da Roit <contact@hdaroit.fr>
2020-03-30 12:24:54 +0200

    Handle pokemon speed equality

--- src/combat.ts
+++ src/combat.ts
@@ -1,10 +1,22 @@
 import { Pokemon } from './Pokemon';
 
+/**
+ * Get first attacker for a combat
+ * @param firstPokemon
+ * @param secondPokemon
+ * @param random inject function that returns a number between 0 and 1
+ */
 export function getFirstAttacker(
   firstPokemon: Pokemon,
   secondPokemon: Pokemon,
+  random?: () => number,
 ): Pokemon {
-  return firstPokemon.speed > secondPokemon.speed
-    ? firstPokemon
-    : secondPokemon;
+  if (firstPokemon.speed > secondPokemon.speed) {
+    return firstPokemon;
+  } else if (firstPokemon.speed < secondPokemon.speed) {
+    return secondPokemon;
+  }
+
+  const randomFn = random ?? Math.random;
+  return randomFn() > 0.5 ? firstPokemon : secondPokemon;
 }
--- test/combat.spec.ts
+++ test/combat.spec.ts
@@ -2,9 +2,35 @@ import { Pokemon } from '../src/Pokemon';
 import { getFirstAttacker } from '../src/combat';
 
 describe('Combat', () => {
-  it('should determine the first attackers which has the most speed', () => {
+  it('should determine the attackers which has the most speed (1)', () => {
+    const firstPokemon = new Pokemon('Pikachu', 1, 2, 3, 4, 5, 6);
+    const secondPokemon = new Pokemon('Herbizarre', 1, 2, 3, 4, 5, 7);
+    expect(getFirstAttacker(firstPokemon, secondPokemon)).toBe(secondPokemon);
+  });
+
+  it('should determine the attackers which has the most speed (2)', () => {
+    const firstPokemon = new Pokemon('Pikachu', 1, 2, 3, 4, 5, 7);
+    const secondPokemon = new Pokemon('Herbizarre', 1, 2, 3, 4, 5, 6);
+    expect(getFirstAttacker(firstPokemon, secondPokemon)).toBe(firstPokemon);
+  });
+
+  it('should determine the attackers randomly when the speed is the same (1)', () => {
     const firstPokemon = new Pokemon('Pikachu', 1, 2, 3, 4, 5, 6);
     const secondPokemon = new Pokemon('Herbizarre', 1, 2, 3, 4, 5, 6);
-    expect(getFirstAttacker(firstPokemon, secondPokemon)).toBe(secondPokemon);
+    const randomFn = (): number => 0.3;
+
+    expect(getFirstAttacker(firstPokemon, secondPokemon, randomFn)).toBe(
+      secondPokemon,
+    );
+  });
+
+  it('should determine the attackers randomly when the speed is the same (2)', () => {
+    const firstPokemon = new Pokemon('Pikachu', 1, 2, 3, 4, 5, 6);
+    const secondPokemon = new Pokemon('Herbizarre', 1, 2, 3, 4, 5, 6);
+    const randomFn = (): number => 0.7;
+
+    expect(getFirstAttacker(firstPokemon, secondPokemon, randomFn)).toBe(
+      firstPokemon,
+    );
   });
 });
